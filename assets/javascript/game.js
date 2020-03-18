3d107b0aecd970c971d28f0c6b65abf01b25006d
Melanson <Lee.Melanson@LibertyMutual.com>
2020-03-18 15:21:54 -0400

    almostDone

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,122 +1,149 @@
 'use strict'
 
-let obi_wan = {
-  health_points: "120",
-  attack_power: "10",
-  counter_attack_power: "10",
-  friendly_name: "Obi Wan"
-};
+var characters =
+  [{
+    id: "0",
+    health_points: "120",
+    attack_power: "8",
+    counter_attack_power: "15",
+    name: "Obi Wan",
+    image: 'assets/images/obi_wan.jpg',
+    dead_image: 'assets/images/obi_wan_dead.jpg'
+  },
 
-let luke_skywalker = {
-  health_points: "100",
-  attack_power: "20",
-  counter_attack_power: "20",
-  friendly_name: "Luke Skywalker"
-};
+  {
+    id: "1",
+    health_points: "125",
+    attack_power: "12",
+    counter_attack_power: "5",
+    name: "Luke Skywalker",
+    image: 'assets/images/luke.jpg',
+    dead_image: 'assets/images/luke_dead.jpg'
+  },
 
-let darth_sidious = {
-  health_points: "150",
-  attack_power: "30",
-  counter_attack_power: "30",
-  friendly_name: "Darth Sidious"
-};
+  {
+    id: "2",
+    health_points: "150",
+    attack_power: "8",
+    counter_attack_power: "20",
+    name: "Darth Sidious",
+    image: 'assets/images/darth_sidious.jpg',
+    dead_image: 'assets/images/darth_sidious_dead.jpg'
+  },
 
-let darth_maul = {
-  health_points: "100",
-  attack_power: "40",
-  counter_attack_power: "40",
-  friendly_name: "Darth Maul"
-};
+  {
+    id: "3",
+    health_points: "185",
+    attack_power: "7",
+    counter_attack_power: "25",
+    name: "Darth Maul",
+    image: 'assets/images/darth_maul.jpg',
+    dead_image: 'assets/images/darth_maul_dead.jpg'
+  }];
 
-let my_character = "";
-let my_opponent = "";
-let attack_damage = 0;
-let counter_attack_damage = 0;
+var my_character = "";
+var my_character_index = "";
+var my_opponent = "";
+var my_opponent_index = "";
+var my_attack_power = 0;
+var my_counter_attack_power = 0;
+var opponent_attack_power = 0;
+var opponent_counter_attack_power = 0;
 
-$(document).ready(function () {
-  console.log("beginJS");
-  SetupGame();
-  $(".available_character").on("click", SelectPlayer);
-  $("#attack_button").on("click", Attack);
-  $("#enemies").on('click', '.available_enemy', SelectEnemy);
-  $("#reset_button").on('click', ResetGame);
-  
-  //FUNCTIONS
- 
-  function SetupGame() {
-    my_character = "";
-    my_opponent = "";
-    $("#obi_wan_hp").text(obi_wan.health_points);
-    $("#luke_skywalker_hp").text(luke_skywalker.health_points);
-    $("#darth_sidious_hp").text(darth_sidious.health_points);
-    $("#darth_maul_hp").text(darth_maul.health_points);
-  }
-  
-  function SelectPlayer() {
+
+var game = {
+
+  CreateCharacterCards: function (i) {
+    $('#available_chars').append("<div class='available_character'" + "id=" + characters[i].id + ">" + "<img src=" + characters[i].image + ">" + "<p>" + characters[i].health_points + "</p>" + "</div>")
+  },
+
+  SetupGame: function () {
+    var i;
+    for (i = 0; i < characters.length; i++) {
+      game.CreateCharacterCards(i);
+    }
+    console.log("character cards created");
+
+  },
+
+  SelectPlayer: function () {
     $(".available_character").off();
-    my_character = $(this).attr("id");
-    console.log("my character selected");
+    my_character_index = $(this).attr("id");
+    my_attack_power = characters[my_character_index].attack_power
     $(this).removeClass();
     $(this).addClass("selected_character");
     $(".selected_character").appendTo(".my_character");
     $(".available_character").addClass("available_enemy");
     $(".available_enemy").appendTo("#enemies");
     $(".available_enemy").removeClass("available_character");
-   
-  }
-  
-  function SelectEnemy() {
+  },
+
+  SelectEnemy: function () {
     $('#enemies').off('click', '.available_enemy');
-    my_opponent = $(this).attr("id");
+    my_opponent_index = $(this).attr("id");
+    opponent_attack_power = characters[my_opponent_index].attack_power
     console.log("opponent selected");
     $(this).removeClass();
     $(this).addClass("opponent");
-    $(".opponent").appendTo(".opponent_section");
+    $(".opponent").appendTo("#my_opponent");
     $("#attack_button").toggleClass("button_visible");
-  }
+    $("#attack_result").text("Enemy selected. Press Attack button to attack.");
+  },
 
-  function Attack() {
-    console.log("attack!");
-   let str_attack_message = my_character + " attacks, causing " + attack_damage + " damage. " + my_opponent + " counterattacks, causing " + counter_attack_damage + " damage.";
-   $("#attack_result").text(str_attack_message);
+  Attack: function () {
+    console.log("attacking");
+    let str_attack_message = characters[my_character_index].name + " attacks, causing " + my_attack_power + " damage. " + characters[my_opponent_index].name + " counterattacks, causing " + characters[my_opponent_index].counter_attack_power + " damage.";
+    $("#attack_result").text(str_attack_message);
+    game.CalculateHP();
+  },
 
-  
-
-    //display the damage inflicted on both from this strike
-    //update the remaining HP in the game pieces
-    //attack power increases?
-    //if I dip below zero on counter, I lose
-    //if enemy defeated, hide their game piece
-  }
+  CalculateHP: function () {
+    characters[my_character_index].health_points = characters[my_character_index].health_points - opponent_attack_power;
+    characters[my_opponent_index].health_points = characters[my_opponent_index].health_points - my_attack_power;
     
+    //my player has been defeated after strike
+    if (characters[my_character_index].health_points <= 0) {    
+      characters[my_character_index].health_points = 0;
+      $('#my_character').empty().append("<div class='selected_character'" + "id=" + characters[my_character_index].id + ">" + "<img src=" + characters[my_character_index].dead_image + ">" + "<p>" + characters[my_character_index].health_points + "</p>" + "</div>")
+      $("#attack_button").toggleClass("button_visible");
+      $("#attack_result").text("Your player had been defeated. Press Restart to play again");
+      return;
+    }
     
+    //enemy is defeated after strike
+    else if (characters[my_opponent_index].health_points <= 0) {      
+      characters[my_opponent_index].health_points = 0
+      $('#my_opponent').empty();
+      $('#my_character').empty().append("<div class='selected_character'" + "id=" + characters[my_character_index].id + ">" + "<img src=" + characters[my_character_index].image + ">" + "<p>" + characters[my_character_index].health_points + "</p>" + "</div>")
+      $("#attack_result").text("You have defeated " + characters[my_opponent_index].name + ". Select a new enemy to continue");
+      $("#attack_button").toggleClass("button_visible");
 
+     //no enemies remain, game over
+      if ($('.available_enemy').length == 0) {
+        console.log("game over");
+        $("#attack_result").text("You Win!!! Press Restart to play again");
+        return;
+      }
+      $("#enemies").on('click', '.available_enemy', game.SelectEnemy);
+    }
 
-  function ResetGame() {
+    //my player and enemy are still alive after strike
+    else {
+      $('#my_character').empty().append("<div class='selected_character'" + "id=" + characters[my_character_index].id + ">" + "<img src=" + characters[my_character_index].image + ">" + "<p>" + characters[my_character_index].health_points + "</p>" + "</div>")
+      $('#my_opponent').empty().append("<div class='opponent'" + "id=" + characters[my_opponent_index].id + ">" + "<img src=" + characters[my_opponent_index].image + ">" + "<p>" + characters[my_opponent_index].health_points + "</p>" + "</div>")
+      my_attack_power = parseInt(my_attack_power, 10) + parseInt(characters[my_character_index].attack_power, 10)
+    }
+  },
+
+  ResetGame: function () {
     location.reload();
   }
+};
 
+$(document).ready(function () {
+  game.SetupGame();
+  $(".available_character").on("click", game.SelectPlayer);
+  $("#attack_button").on("click", game.Attack);
+  $("#enemies").on('click', '.available_enemy', game.SelectEnemy);
+  $("#reset_button").on('click', game.ResetGame);
 });
-
-  /*
-  
-		
-	
-	
-			§ Whenever the player clicks attack, their character damages the defender. The opponent will lose HP (health points). These points are displayed at the bottom of the defender's picture.
-			§ The opponent character will instantly counter the attack. When that happens, the player's character will lose some of their HP. These points are shown at the bottom of the player character's picture.
-	3. The player will keep hitting the attack button in an effort to defeat their opponent.
-		○ When the defender's HP is reduced to zero or below, remove the enemy from the defender area. The player character can now choose a new opponent.
-	4. The player wins the game by defeating all enemy characters. The player loses the game the game if their character's HP falls to zero or below.
-	
-
-	• Each time the player attacks, their character's Attack Power increases by its base Attack Power.
-		○ For example, if the base Attack Power is 6, each attack will increase the Attack Power by 6 (12, 18, 24, 30 and so on).
-	• The enemy character only has Counter Attack Power.
-		○ Unlike the player's Attack Points, Counter Attack Power never changes.
-	• The Health Points, Attack Power and Counter Attack Power of each character must differ.
-	•
-		○ A winning player must pick their characters wisely by first fighting an enemy with low Counter Attack Power. This will allow them to grind Attack Power and to take on enemies before they lose all of their Health Points. Healing options would mess with this dynamic.
-	• Your players should be able to win and lose the game no matter what character they choose. The challenge should come from picking the right enemies, not choosing the strongest player.
-
-  */
--- assets/javascript/game_backup.js
+++ assets/javascript/game_backup.js
@@ -1,99 +1,155 @@
 'use strict'
 
-
-let obi_wan = {
+var characters = 
+  [{
+  id: "1",
   health_points: "120",
-  attack_power: "",
-  counter_attack_power: "",
-};
+  attack_power: "10",
+  counter_attack_power: "10",
+  name: "Obi Wan"
+},
 
-let luke_skywalker = {
+{
+  id: "2",
   health_points: "100",
-  attack_power: "",
-  counter_attack_power: "",
-};
+  attack_power: "20",
+  counter_attack_power: "20",
+  name: "Luke Skywalker"
+},
 
-let darth_sidious = {
+  {
+  id: "3",
   health_points: "150",
-  attack_power: "",
-  counter_attack_power: "",
-};
+  attack_power: "30",
+  counter_attack_power: "30",
+  name: "Darth Sidious"
+},
 
-let darth_maul = {
+  {
+  id: "4",
   health_points: "100",
-  attack_power: "",
-  counter_attack_power: "",
-};
+  attack_power: "40",
+  counter_attack_power: "40",
+  name: "Darth Maul"
+}];
+ 
 
 
-var my_character = "";
+let my_character ="";
+let my_opponent = "";
+let attack_damage = 0;
+let counter_attack_damage = 0;
 
 
-function SelectEnemy() {
-  //moves enemy to defender section/ set color to black
+var game = {
 
+  SelectPlayer2 : function(selectedCharacterId){
+      var character;
+      console.log("yes");
+      for(var index in characters){
+          var currentCharacter = characters[index];
+          if(selectedCharacterId === currentCharacter.id){
+              character = characters[index];
+          }
+      }
+
+      return character;
+  },
+   
 }
-
-function Attack() {
-  //ensure there is a selected enemy first
-  //display the damage inflicted on both from this strike
-  //update the remaining HP in the game pieces
-  //attack power increases?
-  //if I dip below zero on counter, I lose
-  //if enemy defeated, hide their game piece
-}
-
-
-function ResetGame() {
-  //move all pieces, scores back to the start, reset colors to white
-}
-
 
 
 $(document).ready(function () {
   console.log("beginJS");
-
-  //Select Character Action
-
+  SetupGame();
+  $(".available_character").on("click", SelectPlayer);
+  $("#attack_button").on("click", Attack);
+  $("#enemies").on('click', '.available_enemy', SelectEnemy);
+  $("#reset_button").on('click', ResetGame);
   
-  $(".available_character").click(function () {
-   
-    if(my_character) {
-      console.log("already selected");
-      return;
-    }
+  //FUNCTIONS
+ 
+  function SetupGame() {
+    characters.my_character = "";
+    my_opponent = "";
+   // $("#obi_wan_hp").text(characters.obi_wan.health_points);
+   // $("#luke_skywalker_hp").text(characters.luke_skywalker.health_points);
+   // $("#darth_sidious_hp").text(characters.darth_sidious.health_points);
+   // $("#darth_maul_hp").text(characters.darth_maul.health_points);
+  }
+  
+  function SelectPlayer() {
+
+
+
+
+    $(".available_character").off();
+   var my_character_index = $(this).attr("id");
+    //characters.my_character.attack_power = $(this).attr("id").attack_power;
+   // console.log(characters.my_character.attack_power);
     
-    my_character = $(this).attr("id");
-    console.log($(this).attr("class"));
     $(this).removeClass();
     $(this).addClass("selected_character");
     $(".selected_character").appendTo(".my_character");
     $(".available_character").addClass("available_enemy");
     $(".available_enemy").appendTo("#enemies");
     $(".available_enemy").removeClass("available_character");
-   console.log($(".available_enemy"));
-  });
+   
+  }
+  
+  function SelectEnemy() {
+    $('#enemies').off('click', '.available_enemy');
+    my_opponent = $(this).attr("id");
+    console.log("opponent selected");
+    $(this).removeClass();
+    $(this).addClass("opponent");
+    $(".opponent").appendTo(".opponent_section");
+    $("#attack_button").toggleClass("button_visible");
+  }
+
+  function Attack() {
+   console.log("attacking");
+
+   let str_attack_message = characters.my_character + " attacks, causing " + attack_damage + " damage. " + my_opponent + " counterattacks, causing " + counter_attack_damage + " damage.";
+   $("#attack_result").text(str_attack_message);
+
+  
+
+    //display the damage inflicted on both from this strike
+    //update the remaining HP in the game pieces
+    //attack power increases?
+    //if I dip below zero on counter, I lose
+    //if enemy defeated, hide their game piece
+  }
+    
+    
 
 
- 
-  $("available_enemy").click(function () {
-    var my_enemy = $(this).attr("id");
-    console.log("clicked enemy");
-    //$(this).removeClass('available_character');
-   // $(this).addClass('selected_character');
-   // $(".selected_character").appendTo("#my_character");
-   // $(".available_character").addClass("available_enemies");
-   // $(".available_character").removeClass("available_character");
-   // $(".available_enemies").appendTo("#enemies");
+  function ResetGame() {
+    location.reload();
+  }
 
-  });
+});
 
- 
- $('#attack_button').click(function () {
+  /*
+  
+		
+	
+	
+			§ Whenever the player clicks attack, their character damages the defender. The opponent will lose HP (health points). These points are displayed at the bottom of the defender's picture.
+			§ The opponent character will instantly counter the attack. When that happens, the player's character will lose some of their HP. These points are shown at the bottom of the player character's picture.
+	3. The player will keep hitting the attack button in an effort to defeat their opponent.
+		○ When the defender's HP is reduced to zero or below, remove the enemy from the defender area. The player character can now choose a new opponent.
+	4. The player wins the game by defeating all enemy characters. The player loses the game the game if their character's HP falls to zero or below.
+	
 
-  //insert attack steps here
-  console.log("attack!");
-       
-    });
+	• Each time the player attacks, their character's Attack Power increases by its base Attack Power.
+		○ For example, if the base Attack Power is 6, each attack will increase the Attack Power by 6 (12, 18, 24, 30 and so on).
+	• The enemy character only has Counter Attack Power.
+		○ Unlike the player's Attack Points, Counter Attack Power never changes.
+	• The Health Points, Attack Power and Counter Attack Power of each character must differ.
+	•
+		○ A winning player must pick their characters wisely by first fighting an enemy with low Counter Attack Power. This will allow them to grind Attack Power and to take on enemies before they lose all of their Health Points. Healing options would mess with this dynamic.
+	• Your players should be able to win and lose the game no matter what character they choose. The challenge should come from picking the right enemies, not choosing the strongest player.
 
-  });
+  */
